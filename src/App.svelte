79ebf05e6023815375c0360fe41af74f0451e279
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-07-26 13:18:51 +0530

    added more functionality and UI Interactivness

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,25 @@
 <script lang="ts">
-    let likeCount = 0;
+    let totalLikesCount = 0;
+    let likesLimit = 10
+    let taskRunning = false
+
+    
+
+    function handleClick() {
+        if (taskRunning){
+            // A Message to Service Worker to Stop the Task Loop wiith
+            // Max time, Min Time, Likes Limit
+        }else{
+            // A Messgae to Service WOrker to Start the Task Loop
+        }
+
+        // Flipping the boolean
+        taskRunning = !taskRunning
+        console.log("Started");
+    }
+
+    // A Event Listner to Listen for Likes Limit Hit and flip the taskRunnig Bollean
+    
 </script>
 
 <main
@@ -8,17 +28,23 @@
     <div class="stats shadow stats-vertical w-full">
         <div class="stat">
             <div class="stat-title">Total Likes</div>
-            <div class="stat-value ">{likeCount}</div>
+            <div class="stat-value relative ">
+                {totalLikesCount}
+                <div class:block={!taskRunning && totalLikesCount > 0} class:hidden={taskRunning || totalLikesCount == 0} class="stat-value top-0 absolute animate-ping text-success">{totalLikesCount}</div>
+            </div>
+            
+            <div class="stat-desc">Today</div>
         </div>
 
         <div class="stat">
             <div class="stat-title">Status</div>
-            <div class="stat-value text-success text-3xl">Running</div>
+            <div class="stat-value text-3xl" class:animate-pulse={taskRunning}  class:text-success={taskRunning} class:text-error={!taskRunning}>{taskRunning?"Running":"Stopped"}</div>
+            <div class="stat-desc">On Current Page</div>
         </div>
 
     </div>
 
-    <button type="button" class="btn btn-success">Start</button>
+    <button type="button" class="btn capitalize " class:btn-error={taskRunning} class:btn-success={!taskRunning}  on:click={handleClick} >{taskRunning?"Stop":"Start"}</button>
     <label class="form-control w-full max-w-xs">
         <div class="label">
             <span class="label-text">Likes Limit</span>
@@ -27,6 +53,7 @@
             type="text"
             placeholder="Type here"
             class="input input-bordered w-full max-w-xs"
+            bind:value={likesLimit}
         />
     </label>
 </main>
