b8b5080ee1af218697f56c3c4b0fbe93a0895361
Keshav-writes-code <dhimankeshav201@gmail.com>
2024-07-27 11:01:28 +0530

    added task Status Variable, code formating, changed message titles

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -2,66 +2,83 @@ let totalLikesCount = 0;
 let likesLimit = 0;
 
 function likeTaskLoop(maxTime: number, minTime: number, likesLimit: number) {
+    let taskRunning = true
     let randTime = Math.floor(Math.random() * (maxTime - minTime) + minTime);
-
     let timeOutId = setTimeout(() => {
-        let likeElem = document.querySelector(
-            'svg[aria-label="Like"]'
-        );
+        let likeElem = document.querySelector('svg[aria-label="Like"]');
         if (!likeElem) {
-            let rafId = 0
-            let targetFPS:number = 1
-            let lastTimeStamp:number = 0
+            let rafId = 0;
+            let targetFPS: number = 1;
+            let lastTimeStamp: number = 0;
             let check = (timeStamp: number) => {
-                if (timeStamp - lastTimeStamp <= (1000/targetFPS)) {rafId = requestAnimationFrame(check); return}
+                if (timeStamp - lastTimeStamp <= 1000 / targetFPS) {
+                    rafId = requestAnimationFrame(check);
+                    return;
+                }
                 lastTimeStamp = timeStamp;
 
-                likeElem = document.querySelector('svg[aria-label="Like"]')
+                likeElem = document.querySelector('svg[aria-label="Like"]');
                 if (!likeElem) {
-                    window.scrollTo({top: document.body.scrollHeight, left: 0, behavior:"smooth"});
-                    let loadingElem = document.querySelector('svg[aria-label="Loading..."]')
+                    window.scrollTo({
+                        top: document.body.scrollHeight,
+                        left: 0,
+                        behavior: "smooth",
+                    });
+                    let loadingElem = document.querySelector(
+                        'svg[aria-label="Loading..."]'
+                    );
                     if (!loadingElem) {
-                        return "reached End of page"
-                    }else{
-                        rafId = requestAnimationFrame(check)
+                        return "reached End of page";
+                    } else {
+                        rafId = requestAnimationFrame(check);
                     }
                 }
-            }
-            requestAnimationFrame(check)
-        };
+            };
+            requestAnimationFrame(check);
+        }
         if (!likeElem) return;
         likeElem.scrollIntoView({ behavior: "smooth" });
         likeElem.dispatchEvent(new MouseEvent("click", { bubbles: true }));
 
         chrome.runtime.sendMessage({
             type: "data",
-            title: "Did a like"
+            title: "Did a like",
         });
 
         likeTaskLoop(maxTime, minTime, likesLimit);
     }, randTime);
-    
-    chrome.runtime.onMessage.addListener(({ type, title, ...data }) => {
-        if (type == "action") {
-            if (title == "Stop Likes") {
-                clearTimeout(timeOutId);
+
+    chrome.runtime.onMessage.addListener(
+        ({ type, title, ...data }, _, sendResponse) => {
+            if (type == "action") {
+                if (title == "Stop Likes Task") {
+                    clearTimeout(timeOutId);
+                    taskRunning = false;
+                }   
+            }
+            if (type == "data") {
+                if (title == "give me task status") {
+                    sendResponse({
+                        taskRunning: taskRunning,
+                    });
+                }
             }
         }
-    })
+    );
 }
 
 function stopAllLikeTasksLoops() {
     const manifest = chrome.runtime.getManifest();
     if (manifest.host_permissions) {
         const urlPatterns = manifest.host_permissions;
-        
+
         // Query for tabs matching these URL patterns
-        chrome.tabs.query({url: urlPatterns}, function(tabs) {
+        chrome.tabs.query({ url: urlPatterns }, function (tabs) {
             for (let tab of tabs) {
                 if (!tab.id) return;
                 chrome.tabs.sendMessage(tab.id, {
                     type: "action",
-                    title: "Stop Likes"
+                    title: "Stop Likes Task",
                 });
             }
         });
@@ -86,33 +103,45 @@ function sendTargetLikeReached_updateToPopup() {
     chrome.runtime.sendMessage({ type: "data", title: "Target Like Reached" });
 }
 
-chrome.runtime.onMessage.addListener(({ type, title, ...data }, _, sendResponse) => {
-    switch (type) {
-        case "action":
-            switch (title) {
-                case "Start Likes":
-                    likesLimit = data.likesLimit;
-                    injectAutomaticLikerInCurrentPage(data.maxTime, data.minTime, likesLimit);
-                    break;
-                case "Stop Likes":
-                    stopAllLikeTasksLoops();
-                    break;
-                case "Give me Likes":
-                    sendResponse({likes: totalLikesCount});
-                    break;
-            }
-            break;
-        case "data":
-            switch (title) {
-                case "Did a like":
-                    totalLikesCount++;
-                    chrome.runtime.sendMessage({ type: "data", title: "Like Count", data: totalLikesCount });
-                    if (totalLikesCount >= likesLimit) {
+chrome.runtime.onMessage.addListener(
+    async ({ type, title, ...data }, _, sendResponse) => {
+        switch (type) {
+            case "action":
+                switch (title) {
+                    case "Start Liking":
+                        likesLimit = data.likesLimit;
+                        injectAutomaticLikerInCurrentPage(
+                            data.maxTime,
+                            data.minTime,
+                            likesLimit
+                        );
+                        break;
+                    case "Stop Liking":
                         stopAllLikeTasksLoops();
-                        sendTargetLikeReached_updateToPopup()
-                    }
-                    break;
-            }
-            break;
+                        break;
+                    
+                }
+                break;
+            case "data":
+                switch (title) {
+                    case "Did a like":
+                        totalLikesCount++;
+                        chrome.runtime.sendMessage({
+                            type: "data",
+                            title: "Like Count",
+                            data: totalLikesCount,
+                        });
+                        if (totalLikesCount >= likesLimit) {
+                            stopAllLikeTasksLoops();
+                            sendTargetLikeReached_updateToPopup();
+                        }
+                        break;
+                    case "give me state data":
+                        sendResponse({likes: totalLikesCount});
+                        
+                        break;
+                }
+                break;
+        }
     }
-});
+);
